5b6c3023d8a1407fbc02cdcef6cf1ddf47e6924e
Akorlie Edward Tsatsu <65318539+edwardtsatsu@users.noreply.github.com>
2021-07-29 22:48:17 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,17 +1,34 @@
+#importing python library
 from flask import (Flask,redirect,render_template,request,session,url_for,jsonify) 
+from flaskext.mysql import MySQL
 import json
 import requests
 from base64 import *
 
 
-
+#initializing flask app
 app = Flask(__name__)
         
+#creating sql object
+mysql = MySQL()
 
+#MySQL configuration
+app.config['MYSQL_DATABASE_USER'] = 'root'
+app.config['MYSQL_DATABASE_PASSWORD'] = ''
+app.config['MYSQL_DATABASE_DB'] = 'simregistration'
+app.config['MYSQL_DATABASE_HOST'] = 'localhost'
+
+mysql.init_app(app)
+
+
+#seting my flask route
 @app.route('/')
 def index():
     return render_template('home.html')
 
+@app.route('/home')
+def home():
+    return render_template('home.html')
 
 
 @app.route('/enrol/', methods=['GET','POST']) 
@@ -24,11 +41,6 @@ def enrol():
 def verify():
     return render_template('verify.html')
 
-
-
-@app.route('/home/', methods=['GET','POST'])
-def home():
-    return render_template('home.html')
 
 
 @app.route('/facial/', methods=['GET','POST'])
@@ -45,7 +57,6 @@ def register():
 
 
 
-
+#running the app and turning debug mode on
 if __name__ == "__main__":
-    app.debug =True
-    app.run() 
+    app.run(debug =True)

8b0d412a80242bdac7bb0ab5254792f928e8da47
Akorlie Edward Tsatsu <65318539+edwardtsatsu@users.noreply.github.com>
2021-07-30 17:47:29 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,11 +1,7 @@
 #importing python library
 from flask import (Flask,redirect,render_template,request,session,url_for,jsonify) 
-from flaskext.mysql import MySQL
-from pymysql import connect
-import pymysql
+from flaskext.mysql  import MySQL
 import re
-import json
-import requests
 from base64 import *
 
 
@@ -13,7 +9,7 @@ from base64 import *
 app = Flask(__name__)
         
 #creating sql object
-mysql = MySQL()
+mysql = MySQL(app)
 
 #MySQL configuration
 app.config['MYSQL_DATABASE_USER'] = 'root'
@@ -21,7 +17,6 @@ app.config['MYSQL_DATABASE_PASSWORD'] = ''
 app.config['MYSQL_DATABASE_DB'] = 'simregistration'
 app.config['MYSQL_DATABASE_HOST'] = 'localhost'
 
-mysql.init_app(app)
 
 
 #seting my flask route
@@ -36,54 +31,24 @@ def home():
 
 @app.route('/enrol/', methods=['GET','POST']) 
 def enrol():
-    #connect to my database
+    if request.method == 'POST':
+        Save = request.form
+        firstname = Save['firstname']
+        lastname = Save['lastname']
+        idtype = Save['idtype']
+        idnumber = Save['idnumber']
+        password = Save['password']
+        rpassword = Save['rpassword']
+        cur = mysql.connection.cursor()
+        cur.execute("INSERT INTO enrol (firstname,lastname,idtype,idnumber,password,rpassword) VALUES (%s,%s,%s,%s,%s,%s)",(firstname,lastname,idtype,idnumber,password,rpassword))
+        mysql.connection.commit()
+        cur.close()
 
-    conn = mysql.connect()
-    cursor = conn.cursor(pymysql.cursors.DictCursor)
+        return "Enrolled Succussfully"
 
-    #output message if something goes wrong
-    msg = "Check your details provided"
-
-    #checking if "user detais" exists 
-    if request.method == "POST" and 'firstname' in request.form and 'lastname'in request.form and 'idtype' in request.form and 'idnumber' in request.form and 'password' in request.form:
-        #create varibales for easy access
-        firstname = request.form['firstname']
-        lastname = request.form['lastname']
-        idtype = request.form['idtype']
-        idnumber = request.form['idnumber']
-        password = request.form['password']
-
-        #check if account exists uinsg MYSQL
-        cursor.execute(
-            'SELECT * FROM enrol WHERE idnumber = %s AND firstname = %s AND idtype = %s', (idnumber,firstname,idtype))
-        account = cursor.fetchone()
-
-        #if account exisit show error and validation checks
-        if account:
-            msg  = 'Account already exists!'
-        elif not re.match(r'[^@]+@[^@]+\.[^@]+',idnumber):
-            msg = 'invalid idnumber'
-        elif not re.match(r'[A-Za-z0-9]+' , firstname):
-            msg = 'firstname and lastname must conatin only charcater and numbers!'
-        elif not idtype or not password or not idnumber:
-            msg = 'Kindly fill the form!'
-        else:
-            cursor.execute('INSERT INTO account VALUES (NULL, %s, %s, %s, %s)', 
-            (firstname,lastname,idtype,idnumber,password))
-            conn.commit()
-
-            msg = 'you have enrol sucesssfully! visit the home page and verify'
-    elif request.method == 'POST':
-        msg = 'Kindly fill the form!'
-    
-    #shows the enrollment form with message if any
-    return render_template('enrol.html', msg = msg)
+    return render_template('enrol.html')
 
             
-
-
-
-
 
 @app.route('/verify/', methods=['GET', 'POST'])
 def verify():

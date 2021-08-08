d5d865c021f510e2a5fe2afdfb28f7760c8e770f
Akorlie Edward Tsatsu <65318539+edwardtsatsu@users.noreply.github.com>
2021-08-09 00:46:43 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,15 +1,23 @@
 #importing python library
 from flask import Flask, redirect, render_template, request, session, url_for
+from flaskext.mysql import MySQL
+import pymysql
+import re
 
 
 #initializing flask app
 app = Flask(__name__)
         
 #creating sql object
-#mysql = MySQL(app)
+mysql = MySQL()
 
 #MySQL configuration
+app.config['MYSQL_DATABASE_USER'] = 'root'
+app.config['MYSQL_DATABASE_PASSWORD'] = ''
+app.config['MYSQL_DATABASE_DB'] = 'simregistration'
+app.config['MYSQL_DATABASE_HOST'] = 'localhost'
 
+mysql.init_app(app)
 
 
 #seting my flask route
@@ -24,19 +32,45 @@ def home():
 
 @app.route("/enrol/", methods=["GET","POST"]) 
 def enrol():
-    if request.method == "POST":
-        req = request.form
-        firstname = req["firstname"]
-        lastname = req["lastname"]
-        idnumber = req["idnumber"]
-        idtype = req["idtype"]
-        password = req["password"]
-        confpassword = req["confpassword"]
-        
-        print(firstname,lastname,idnumber,idtype,password,confpassword)
-        return redirect("/facial")
+    #connect 
+    conn = mysql.connect()
+    cursor = conn.cursor(pymysql.cursors.DictCursor)
+    
+    msg = ""
+    #check for user details
+    if request.method == "POST" and "firstname" in request.form and "lastname" in request.form and "idnumber" in request.form and "idtype" in request.form and "pword" in request.form and "confpword" in request.form:
+        #create variables for each access
+        firstname = request.form["firstname"]
+        lastname = request.form["lastname"]
+        idnumber = request.form["idnumber"]
+        idtype = request.form["idtype"]
+        pword = request.form["pword"]
+        confpword = request.form["confpword"]
 
-    return render_template('enrol.html')
+        #check if account exist in MySQL:
+        cursor.execute(
+            'SELECT * FROM enrol WHERE firstname = %s and lastname = %s and idnumber = %s and idtype = %s and pword = %s and confpword',(firstname, lastname, idnumber, idtype, pword, confpword))
+        account = cursor.fetchone()
+        #if account exists show error and validation checks
+        if account:
+            msg = "Account already exist"
+        elif not re.match(r'[A-Za-z0-9]+',firstname):
+            msg = 'firstname must contain only characters and numbers!'
+        elif not firstname or not idnumber or not pword:
+            msg = 'please fill out the form'
+        else:
+            cursor.execute('INSERT INTO enrol VALUES (NULL, %s, %s, %s, %s, %s)',
+            (firstname, lastname, idnumber, idtype, pword, confpword))
+
+            conn.commit()
+
+            msg = "You have Successfully Registered!"
+    elif request.method == "POST":
+        msg = "Please fill out the form!"
+        #redirect url to facial for the next process
+        #return redirect("/facial")
+
+    return render_template('enrol.html', msg = msg)
 
             
 

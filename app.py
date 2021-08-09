586b901cdd970a0ce2adce91b4af65706436941c
Akorlie Edward Tsatsu <65318539+edwardtsatsu@users.noreply.github.com>
2021-08-09 18:50:25 +0200

    update

--- app.py
+++ app.py
@@ -9,15 +9,13 @@ import re
 app = Flask(__name__)
         
 #creating sql object
-mysql = MySQL()
+mysql = MySQL(app)
 
 #MySQL configuration
 app.config['MYSQL_DATABASE_USER'] = 'root'
 app.config['MYSQL_DATABASE_PASSWORD'] = ''
-app.config['MYSQL_DATABASE_DB'] = 'simregistration'
+app.config['MYSQL_DATABASE_DB'] = 'simcard'
 app.config['MYSQL_DATABASE_HOST'] = 'localhost'
-
-mysql.init_app(app)
 
 
 #seting my flask route
@@ -38,39 +36,23 @@ def enrol():
     
     msg = ""
     #check for user details
-    if request.method == "POST" and "firstname" in request.form and "lastname" in request.form and "idnumber" in request.form and "idtype" in request.form and "pword" in request.form and "confpword" in request.form:
-        #create variables for each access
+    if request.method == "POST": 
         firstname = request.form["firstname"]
         lastname = request.form["lastname"]
-        idnumber = request.form["idnumber"]
         idtype = request.form["idtype"]
+        idnumber = request.form["idnumber"]
         pword = request.form["pword"]
         confpword = request.form["confpword"]
 
         #check if account exist in MySQL:
-        cursor.execute(
-            'SELECT * FROM enrol WHERE firstname = %s and lastname = %s and idnumber = %s and idtype = %s and pword = %s and confpword',(firstname, lastname, idnumber, idtype, pword, confpword))
-        account = cursor.fetchone()
-        #if account exists show error and validation checks
-        if account:
-            msg = "Account already exist"
-        elif not re.match(r'[A-Za-z0-9]+',firstname):
-            msg = 'firstname must contain only characters and numbers!'
-        elif not firstname or not idnumber or not pword:
-            msg = 'please fill out the form'
-        else:
-            cursor.execute('INSERT INTO enrol VALUES (NULL, %s, %s, %s, %s, %s)',
-            (firstname, lastname, idnumber, idtype, pword, confpword))
-
-            conn.commit()
-
-            msg = "You have Successfully Registered!"
-    elif request.method == "POST":
-        msg = "Please fill out the form!"
+        cursor.execute('INSERT INTO enrol VALUES(%s, %s, %s, %s, %s, %s)',(firstname, lastname, idtype, idnumber, pword, confpword))
+        
+        conn.commit()
+        cursor.close()
         #redirect url to facial for the next process
-        #return redirect("/facial")
+        return redirect("/facial")
 
-    return render_template('enrol.html', msg = msg)
+    return render_template('enrol.html')
 
             
 
